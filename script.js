1d82898745e7e731c7904e371b291fa2766a68a7
Kevin Abey Issac <kevinabey04@gmail.com>
2024-07-09 09:23:50 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Welcome");
+// this is a 
+
+let value = 6
+let a=value+5;
+{ 
+    let value=7;
+    console.log("The inside value is", value);
+}
+console.log("The value outside is", value)
+
+
